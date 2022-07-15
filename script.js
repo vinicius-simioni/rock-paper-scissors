daf85b38a64a7e3541ffac052b16f69454e489d6
Vinícius Simioni <viniciussimioni@outlook.com>
2022-07-15 11:00:38 -0300

    getComputerChoice function working

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("hello world")
+function getComputerChoice(){
+    //outputs a random choice between Rock, Paper or Scissors
+    randomNumber = Math.floor(Math.random()*3);
+    switch(randomNumber){
+        case 0:
+            console.log('Rock');
+            break;
+        case 1:
+            console.log('Paper');
+            break;
+        case 2:     
+            console.log('Scissors');
+            break;   
+    }
+}
+getComputerChoice()
