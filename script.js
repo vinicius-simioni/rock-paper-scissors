35b428a9ca0ba80fba511df0bfd90b68e938f985
Vinícius Simioni <viniciussimioni@outlook.com>
2022-07-16 13:14:58 -0300

    function playRound working

--- script.js
+++ script.js
@@ -4,13 +4,43 @@ function getComputerChoice(){
     switch(randomNumber){
         case 0:
             console.log('Rock');
+            return('Rock');
             break;
         case 1:
             console.log('Paper');
+            return('Paper');
             break;
         case 2:     
             console.log('Scissors');
+            return('Scissors');
             break;   
     }
 }
-getComputerChoice()
+
+function playRound(playerSelection, computerSelection){
+    if(playerSelection == 'Rock' && computerSelection == 'Paper'){
+        console.log('You Lose! Paper beats Rock');        
+    }
+    else if(playerSelection == 'Rock' && computerSelection == 'Scissors'){
+        console.log('You Won! Rock beats Scissors');
+    }
+    else if(playerSelection == 'Paper' && computerSelection == 'Scissors'){
+        console.log('You Lose! Scissors beats Paper');
+    }
+    else if(playerSelection == 'Paper' && computerSelection == 'Rock'){
+        console.log('You Won! Paper beats Rock');
+    }
+    else if(playerSelection == 'Scissors' && computerSelection == 'Rock'){
+        console.log('You Lose! Rock beats Scissors');
+    }
+    else if(playerSelection == 'Scissors' && computerSelection == 'Paper'){
+        console.log('You Won! Scissors beats Paper');
+    }
+    else if(playerSelection == 'Scissors' && computerSelection == 'Scissors' || playerSelection == 'Rock' && computerSelection == 'Rock' || playerSelection == 'Paper' && computerSelection == 'Paper'){
+        console.log('Tie! Play again');
+    }
+}
+
+var playerSelection = 'Paper';
+var computerSelection = getComputerChoice();
+playRound(playerSelection, computerSelection);
